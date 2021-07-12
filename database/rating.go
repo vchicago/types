3f2a563e67639df2291273671b118df78c8179b3
Daniel Hawton <daniel@hawton.org>
2021-07-11 20:20:32 -0500

    Fix licensing

--- database/rating.go
+++ database/rating.go
@@ -1,7 +1,25 @@
+/*
+   ZAU Database Typings
+   Copyright (C) 2021  Daniel A. Hawton <daniel@hawton.org>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package database
 
 type Rating struct {
-	ID    int    `json:"id"`
+	ID    int    `json:"id" gorm:"type:int(3)"`
 	Long  string `json:"long" gorm:"type:varchar(25)"`
 	Short string `json:"short" gorm:"type:varchar(3)"`
 }
