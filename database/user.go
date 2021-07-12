637d9510540f35ae7fc7030aa758dfd9b4c99422
Daniel Hawton <daniel@hawton.org>
2021-07-11 19:17:34 -0500

    Fix enum list

--- database/user.go
+++ database/user.go
@@ -7,11 +7,11 @@ type User struct {
 	FirstName         string    `json:"firstname" gorm:"type:varchar(128)"`
 	LastName          string    `json:"lastname" gorm:"type:varchar(128)"`
 	Email             string    `json:"-" gorm:"type:varchar(128);index"`
-	ControllerType    string    `json:"controllerType" gorm:"type:enum('none' 'visitor' 'home')"`
+	ControllerType    string    `json:"controllerType" gorm:"type:enum('none', 'visitor', 'home')"`
 	OperatingInitials string    `json:"operatingInitials" gorm:"type:char(2);index"`
 	RatingID          int       `json:"-"`
 	Rating            Rating    `json:"rating"`
-	Status            string    `json:"status" gorm:"type:enum('none' 'active' 'inactive' 'leave')"`
+	Status            string    `json:"status" gorm:"type:enum('none', 'active', 'inactive', 'leave')"`
 	CreatedAt         time.Time `json:"created_at"`
 	UpdatedAt         time.Time `json:"updated_at"`
 }
